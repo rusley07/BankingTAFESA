f789d2d09931b90ca585d0548ff89690e50f16d2
rusley07 <ruslan.batura@yahoo.com.au>
2017-02-24 11:59:49 +1100

    Add files via upload

---
+++ BankingProject/Controllers/TBankingController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankingProject.Controllers
+{
+    public class TBankingController : Controller
+    {
+        //
+        // GET: /TBanking/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TransferFunds()
+        {
+            return View();
+        }
+        public ActionResult PayBills()
+        {
+            return View();
+        }
+        public ActionResult ViewTransactions()
+        {
+            return View();
+        }
+        public ActionResult AccountDetails(string id)
+        {
+            return Content(id); // View();
+        }
+
+
+    }
+}
